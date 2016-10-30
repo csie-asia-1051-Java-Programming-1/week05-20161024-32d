9fbf834142d8f48895dca4b79c51c7fcba2863f4
32d <105021055@live.asia.edu.tw>
2016-10-30 22:16:48 +0800

    EX

--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -2,14 +2,21 @@
 /*
  * Topic: 讓使用者輸入一正整數n，用while迴圈計算n!後輸出。
  * Date: 2016/10/24
- * Author: 1050210XX 周永振老師
+ * Author: 105021055 王浚維
  */
-
+import java.util.Scanner;
 public class ex02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int m = 1;
+		while(n>1){
+		m=n*m;
+		n--;
+		}
+		System.out.println(m);
 	}
 
 }
